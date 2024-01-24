ecc1a2646daad0cd0ef1c44909f069b236216b34
thousandemon <william@fairfieldco.com>
2024-01-23 22:28:40 -0500

    Refactored Get All Components to implement Buttons to Fetch

--- src/Components/GetAllMessages.js
+++ src/Components/GetAllMessages.js
@@ -1,37 +1,30 @@
 import React, { useEffect, useState} from 'react'
 
 const GetAllMessages = () => {
-    const [messageData, setMessageData] = useState("");
+    const [messageData, setMessageData] = useState(<></>);
 
-    useEffect(() => {
-        fetch("http://localhost:8080/messages")
+    function updateMessages() {
+            fetch("http://localhost:8080/messages")
             .then((resp) => resp.json())
             .then((json) => {
-                setMessageData(() => json);
+                setMessageData(() => json.map((m) =>
+                <div key={m.id} id={'id' + m.id}>
+                    <h5 key = {m.id}>Message ID: {m.id}</h5>
+                    <p key = {m.content}>Message: {m.content}</p>
+                    <h4 key = {m.sender.name}>Sent By: {m.sender.name}</h4>
+                    <br />
+                </div>
+            ));
             })
-    },[])
-
-
-
-    function loadData() {
-        return messageData.map((m) =>
-            <div key={m.id} id={'id' + m.id}>
-                <h5 key = {m.id}>Message ID: {m.id}</h5>
-                <p key = {m.content}>Message: {m.content}</p>
-                <h4 key = {m.sender.name}>Sent By: {m.sender.name}</h4>
-                <br />
-            </div>
-        )
     }
-    if(messageData) {
-        return (
-            loadData()
-        )
-    } else {
-        return (
-            <div>Loading ...</div>
-        )
-    }
+
+    
+    return (
+        <>
+            <button onClick={updateMessages}>Fetch Messages</button>
+            {messageData}
+        </>
+    )
 }
 
 export default GetAllMessages
--- src/Components/GetAllPersons.js
+++ src/Components/GetAllPersons.js
@@ -1,36 +1,31 @@
 import React, { useEffect, useState } from 'react'
 
 const GetAllPersons = () => {
-    const [personData, setPersonData] = useState("");
 
-    useEffect(() => {
+    const [personData, setPersonData] = useState(<></>);
+
+    function updatePeople() {
         fetch("http://localhost:8080/persons")
             .then((resp) => resp.json())
             .then((json) => {
-                setPersonData(() => json);
+                setPersonData(() => json.map((p) => 
+                    <div key={p.id} id={'id' + p.id}>
+                        <h5>Name: {p.name}</h5>
+                        <p>Email: {p.email}</p>
+                        <br />
+                    </div>
+                ))
             })
-    },[])
-
-
-    function loadData() {
-        return personData.map((p) => 
-            <div key={p.id} id={'id' + p.id}>
-                <h5>Name: {p.name}</h5>
-                <p>Email: {p.email}</p>
-                <br />
-            </div>
-        )
     }
 
-    if(personData) {
-        return (
-            loadData()
-        )
-    } else {
-        return (
-            <div>Loading ...</div>
-        )
-    }
+
+    return (
+        <>
+            <button onClick={updatePeople}>Fetch People</button>
+            {personData}
+
+        </>
+    )
 }
 
 export default GetAllPersons
